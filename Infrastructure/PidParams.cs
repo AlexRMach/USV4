04959b72a73223f163c0bb376abdd517b9a4272e
alexvm24 <alex_vm2020@mail.ru>
2025-04-22 21:43:52 +0300

    Добавлена загрузка коэффициентов ПИД из файла

---
+++ Infrastructure/PidParams.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ush4.Infrastructure
+{
+    public class PidParams
+    {
+        
+        public double W { get; set; }
+        public double F { get; set; }
+
+        public double X { get; set; }
+
+        public double uf14 { get; set; }
+        public double uf15 { get; set; }
+        public double uf16 { get; set; }
+        public double uf17 { get; set; }
+        public double uf18 { get; set; }
+        public double uf10 { get; set; }
+        public double uf11 { get; set; }
+        public double uf12 { get; set; }
+        public double uf13 { get; set; }                
+        
+    }
+}
